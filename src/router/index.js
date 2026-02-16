4b7cbb5f20186cfb64b41cac2c481acdfacd5ff2
Yeneviel Roberts <yenevielroberts@gmail.com>
2026-02-16 23:59:56 +0100

    Insert POST funciona

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import Home from '@/views/Home.vue'
 import Canciones from '@/components/Canciones.vue'
 import Peliculas from '@/components/Peliculas.vue'
 import Detalle from '@/views/Detalle.vue'
-import NuevoItem from '@/components/nuevoItem.vue'
+import NewItem from '@/views/NewItem.vue'
 import SinAcceso from '@/views/SinAcceso.vue'
 import Editar from '@/views/Editar.vue'
 
@@ -19,7 +19,7 @@ const router = createRouter({
     {path:"/canciones", name:"Canciones", component:Canciones},
     {path:"/canciones/:id", name:"Detalle",component:Detalle},
     {path:"/editar/:id", name:"Editar",component:Editar},
-    {path:"/canciones/new",name:"nuevoItem", component:NuevoItem},
+    {path:"/canciones/new",name:"nuevoItem", component:NewItem},
     {path:"/peliculas", name:"Peliculas", component:Peliculas},
     {path:"/signup", name:"Signup", component:Signup},
     {path:"/sinAcceso", name:"SinAcceso", component:SinAcceso},
